5c492d9bc48a5d6bea76b703d1e29be8203f45a6
azachbot <zachbot006@gmail.com>
2024-06-01 21:10:54 +0530

    fix

--- uwrapper/scripts/services/login.py
+++ uwrapper/scripts/services/login.py
@@ -1,17 +1,15 @@
 """upstox login"""
-from ...config.config import get_configs
 import urllib.parse
 import requests
 
 def login(apikey,secretkey,rurl):
     """ logging into upstox """
-    config=get_configs()
     rurl_parsed=urllib.parse.quote(rurl,safe="")
-    uri=f'{config["login_url"]}=code&client_id={apikey}&redirect_uri={rurl_parsed}'
+    uri=f'https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id={apikey}&redirect_uri={rurl_parsed}'
     print("Visit the following URL to authorize your application:")
     print(uri)
     code = input("Enter the code: ")
-    url=f"{config['authorization_token_url']}"
+    url=f"https://api.upstox.com/v2/login/authorization/token"
     headers={'accept': "application/json",'Content-Type': "application/x-www-form-urlencoded"}
     data={'code':code,'client_id':apikey,'client_secret':secretkey,'redirect_uri':rurl,'grant_type':'authorization_code'}
     response=requests.post(url,headers=headers,data=data)
